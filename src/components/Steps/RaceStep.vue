39fd55a22edcacf4e8e1ef098679098b15dcad23
Monika <os_mo@wp.pl>
2022-03-30 21:30:37 +0200

    race step feature

---
+++ src/components/Steps/RaceStep.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="race-step">
+    <h2 class="race-step__title">Character Race</h2>
+    <div v-for="(race, index) in races" :key="index">
+      <input
+        type="radio"
+        :id="race.index"
+        :value="race.name"
+        v-model="chosenRace"
+      />
+      <label :for="race.name">{{ race.name }} </label>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  computed,
+  defineComponent,
+  onMounted,
+  ref,
+  Ref,
+  watch
+} from 'vue'
+import { Character } from '@/services/api/Character'
+import { useStore } from 'vuex'
+
+type Race = {
+  index: number
+  name: string
+  url: string
+}
+
+export default defineComponent({
+  name: 'RaceStep',
+  props: {
+    isInputValid: Boolean
+  },
+  setup (props, { emit }) {
+    const races: Ref<Race[]> = ref([])
+    const chosenRace: Ref<string> = ref('')
+
+    const store = useStore()
+    const setCharacterRace = (race: string) => {
+      store.commit('setCharacterRace', race)
+    }
+
+    const getRaces = async () => {
+      try {
+        races.value = await Character.getRaces()
+      } catch (err) {
+        console.error(err)
+      }
+    }
+
+    onMounted(() => {
+      getRaces()
+      chosenRace.value = store.getters.characterRace
+      emit('update:isInputInvalid', Boolean(!chosenRace.value.length))
+    })
+
+    watch(
+      () => chosenRace.value,
+      (currentValue) => {
+        emit('update:isInputInvalid', Boolean(!currentValue.length))
+        setCharacterRace(currentValue)
+      }
+    )
+
+    return {
+      races,
+      chosenRace,
+      characterRace: computed(() => store.state.race)
+    }
+  }
+})
+</script>
+<style scoped lang="scss"></style>
